42cc291112f2d56dff671eb9d114ba4e0c74d212
Olumide <odokedusi@sidmach.com>
2018-05-31 16:24:32 +0100

    Implement application routing

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ const path = require("path");
 const bodyParser = require("body-paresr");
 const cons = require("consolidate");
 const dust = require("dustjs-helpers");
-const pg = require("pg");
+const pg = require('pg')
 
-//DB Connect string
-const connect =
-  "postgresql://olumide:lumidizzle31@database.server.com:3211/recipeapp";
+const app = express();
+
+// const connectionString = "postgres://olumide:lumidizzle31@localhost/recipebookdb";
+const config = {
+  user: 'olumide',
+  database: 'recipebookdb',
+  password: 'lumidizzle31',
+  port: 5432
+};
+
+const pool = new pg.Pool(config);
 
 //Assign Dust engine to .dust files
 app.engine("dust", cons.dust);
@@ -23,10 +31,59 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", function(req, res) {
-  console.log("works");
-  // res.render("index");
+//Routing
+app.get('/', (req, res, next) => {
+  pool.connect(function (err, client, done) {
+      if (err) {
+          console.log("Can not connect to the DB" + err);
+      }
+      client.query('SELECT * FROM recipes', function (err, result) {
+        if (err) {
+          // console.log(err);
+          return res.status(400).send(err);
+        }
+        // console.log(result)
+        return res.render('index', {recipes: result.rows });
+        done();
+      })
+  })
 });
+
+app.post('/add', (req, res) => {
+  pool.connect(function (err, client, done) {
+    if (err) {
+        console.log("Cannot connect to the DB" + err);
+    }
+     client.query('INSERT INTO recipes(name, ingredients, directions) VALUES($1, $2, $3)', [req.body.name, req.body.ingredients, req.body.directions])
+
+     done();
+     res.redirect('/')
+})
+})
+
+app.delete('/delete/:id', (req, res) => {
+  pool.connect(function (err, client, done) {
+    if (err) {
+        console.log("Cannot connect to the DB" + err);
+    }
+     client.query('DELETE FROM recipes WHERE id = $1', [req.params.id])
+
+     done();
+     res.sendStatus(200)
+  })
+})
+
+app.post('/edit', (req, res) => {
+  pool.connect(function (err, client, done) {
+    if (err) {
+        console.log("Cannot connect to the DB" + err);
+    }
+     client.query('UPDATE recipes SET name=$1, ingredients=$2, directions=$3 WHERE id = $4', [req.body.name, req.body.ingredients, req.body.directions, req.body.id])
+
+     done();
+     res.redirect('/')
+})
+})
 
 //Server
 app.listen(3000, function() {
